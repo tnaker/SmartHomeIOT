23ccdfd11232ecff62d9b13dec18540782d98f87
ZaMinVo <vogiaminhstg@gmail.com>
2026-03-28 12:29:25 +0700

    add sleep before connect

--- gateway/src/test_sender.py
+++ gateway/src/test_sender.py
@@ -11,9 +11,14 @@ AIO_KEY = os.getenv("ADAFRUIT_IO_KEY")
 client = MQTT_Control(AIO_USER, AIO_KEY)
 client.connect()
 
+time.sleep(1)
+
 while True:
     import random
     fake_temp = random.randint(25, 30)
+    fake_light = random.randint(1000, 4000)
+    print(f"Đang gửi nhiệt độ giả: {fake_light}")
+    client.publish("bk-iot-light", fake_light)
     print(f"Đang gửi nhiệt độ giả: {fake_temp}")
     client.publish("bk-iot-temp", fake_temp)
     time.sleep(5)
